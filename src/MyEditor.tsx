3240fd3a82d4b0adeb521b15236e4c0b413ae866
Bomby Kitchpanich <bomby@flyntlok.com>
2020-11-12 17:02:05 -0900

    state viewer, RichUtils

--- src/MyEditor.tsx
+++ src/MyEditor.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Editor, EditorState } from 'draft-js';
+import 'es6-shim';
+import React, { useState } from 'react';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 import 'draft-js/dist/Draft.css';
-// import styles from './MyEditor.module.css';
+import styles from './MyEditor.module.css';
 
 function MyEditor(): JSX.Element {
-  return <div>editor component</div>;
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+
+  // const dropUndoRedoStacks = (): void => {
+  //   // editorState maintains arrays undoStack and redoStack, not needed onSubmit
+  //   // return editorState obj with empty undo/redo arrays
+  // };
+
+  function onChange(state: any) {
+    setEditorState(state);
+  }
+
+  function onBoldClick() {
+    onChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+  }
+
+  return (
+    <div>
+      editor: <button onClick={onBoldClick}>B</button>
+      <div className={styles.editorContainer}>
+        <Editor editorState={editorState} onChange={setEditorState} />
+      </div>
+      <hr />
+      editorState: {JSON.stringify(editorState)}
+    </div>
+  );
 }
 
 export default MyEditor;
